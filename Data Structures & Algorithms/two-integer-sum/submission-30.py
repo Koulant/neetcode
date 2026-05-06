914cbeeabc2db13cac6eb1e62e8c5d7c48f7ece1
Anton Koulikov <117700254+Koulant@users.noreply.github.com>
2026-05-06 13:53:18 -0600

    Add: two-integer-sum - submission-30

---
+++ Data Structures & Algorithms/two-integer-sum/submission-30.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # One pass hash map approach
+        num_index = {}
+
+        for i, n in enumerate(nums):
+
+            diff = target - n # Find the complement
+
+            if diff in num_index: # If complement is in the hash map, return complement index, i
+                return [num_index[diff], i]
+
+            num_index[n] = i # Add the number : index mapping we already checked to hash map
+            # This way, if we already passed the complement, we know both indicies in a single pass
+        return []
+
+        
+
+
+
+        

057bda72b6128fa0ff463a8b4d2798207c0351fe
Anton Koulikov <117700254+Koulant@users.noreply.github.com>
2026-05-06 19:46:49 -0600

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Key insight: Can do this in O(n) time using prefix/suffix approach
+        # Product will be whats to the left of i and whats to the right of i
+        # Iterate nums with index
+        # Fill results with prefix product (left of i)
+        # Multiple suffix product in (right of i)
+        # Get product as you go 
+        # Return result
+        len_nums = len(nums)
+        result = [1] * len_nums
+
+        prefix = 1
+        for i in range(len_nums):
+            result[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(len_nums - 1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
+
+        return result
+
+        # Time: O(n)
+        # Space: O(1) extra space 
+        
